34000cef2b53474d0412547a76651012ffe2c89d
WenBaiyun-11 <0wen.raiwenbal01@gmail.com>
2024-05-17 06:32:10 +0800

    changed script path

--- modules/MMM-LookingGlass/node_helper.js
+++ modules/MMM-LookingGlass/node_helper.js
@@ -9,11 +9,11 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         switch (notification) {
             case "COLOR_FILL":
-                spawn('sudo python3', ['./utils/neoPixelSpydev.py', payload.r, payload.g, payload.b, payload.ledCount]).stdout.on('data', data => this.sendSocketNotification('debug', data.toString()));
+                spawn('sudo python3', [`${this.path}/utils/neoPixelSpydev.py`, payload.r, payload.g, payload.b, payload.ledCount]).stdout.on('data', data => this.sendSocketNotification('debug', data.toString()));
                 console.log('LED IS TURNED ON!');
                 break;
             case "TURN_OFF":
-                spawn('sudo python3', ['./utils/neoPixelSpydev.py', 0, 0, 0, payload.ledCount]).stdout.on('data', data => this.sendSocketNotification('debug', data.toString()));
+                spawn('sudo python3', [`${this.path}/utils/neoPixelSpydev.py`, 0, 0, 0, payload.ledCount]).stdout.on('data', data => this.sendSocketNotification('debug', data.toString()));
                 console.log('LED IS TURNED OFF!');
                 break;
             default:
