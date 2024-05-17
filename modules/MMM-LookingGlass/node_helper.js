c94b25684cf29f8ad84f144ff8f2863e196dce23
WenBaiyun-11 <0wen.raiwenbal01@gmail.com>
2024-05-17 08:39:20 +0800

    Major update

--- modules/MMM-LookingGlass/node_helper.js
+++ modules/MMM-LookingGlass/node_helper.js
@@ -1,6 +1,5 @@
 const NodeHelper = require("node_helper");
-const exec = require('child_process').exec
-const pyShell = require('python-shell')
+const cmd = require('node-cmd')
 
 module.exports = NodeHelper.create({
     start: function () {
@@ -10,17 +9,17 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         switch (notification) {
             case "COLOR_FILL":
-                pyShell.PythonShell.run(`${this.path}/utils/neoPixelSpydev.py`, { args : [`${payload.r}`,`${payload.g}`,`${payload.b}`,`${payload.ledCount}`]}).then(msg => {
+/*                 pyShell.PythonShell.run(`utils/neoPixelSpydev.py`, { args : [`${payload.r}`,`${payload.g}`,`${payload.b}`,`${payload.ledCount}`]}).then(msg => {
                     console.log(msg);
-                })
-                //exec(`python3 ".${this.path}/utils/neoPixelSpydev.py" ${payload.r} ${payload.g} ${payload.b} ${payload.ledCount}`);
+                }) */
+                cmd.run(`sudo python3 ".${this.path}/utils/neoPixelSpydev.py" ${payload.r} ${payload.g} ${payload.b} ${payload.ledCount}`);
                 console.log('LED IS TURNED ON!');
                 break;
             case "TURN_OFF":
-                //exec(`python3 ".${this.path}/utils/neoPixelSpydev.py" 0 0 0 ${payload.ledCount}`);
-                pyShell.PythonShell.run(`${this.path}/utils/neoPixelSpydev.py`, { args : [`0`,`0`,`0`,`${payload.ledCount}`]}).then(msg => {
+                cmd.run(`sudo python3 ".${this.path}/utils/neoPixelSpydev.py" 0 0 0 ${payload.ledCount}`);
+/*                 pyShell.PythonShell.run(`utils/neoPixelSpydev.py`, { args : [`0`,`0`,`0`,`${payload.ledCount}`]}).then(msg => {
                     console.log(msg);
-                })
+                }) */
                 console.log('LED IS TURNED OFF!');
                 break;
             default:
